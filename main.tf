cb41d37730c1b1fb5843faaef694402c770202c5
James Phillips <phillips.james@gmail.com>
2021-05-16 08:58:12 -0500

    adding subnets and associated variables. content shamelessly stolen from hashi's vpc module

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+locals {
+  max_subnet_length = max(
+    length(var.private_subnets),
+    length(var.isolated_subnets)
+  )
+
+  nat_gateway_count = var.enable_nat_gateway ? 0 : length(var.azs)
+
+  # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
+  vpc_id = element(
+    concat(
+      aws_vpc_ipv4_cidr_block_association.this.*.vpc_id,
+      aws_vpc.this.*.id,
+      [""],
+    ),
+    0,
+  )
+}
+
 resource "aws_vpc" "this" {
   count = var.create_vpc ? 1 : 0
 
@@ -14,8 +33,19 @@ resource "aws_vpc" "this" {
     var.vpc_tags,
   )
 }
+
+resource "aws_vpc_ipv4_cidr_block_association" "this" {
+  count = var.create_vpc && length(var.secondary_cidr_blocks) > 0 ? length(var.secondary_cidr_blocks) : 0
+
+  vpc_id = aws_vpc.this[0].id
+
+  cidr_block = element(var.secondary_cidr_blocks, count.index)
+}
+
 # manage the default security group, no ingress or egress mean no rules
 resource "aws_default_security_group" "this" {
+  count = var.create_vpc ? 1 : 0
+
   vpc_id = aws_vpc.this[0].id
   tags = {
     "Name" = "default"
@@ -24,6 +54,8 @@ resource "aws_default_security_group" "this" {
 
 #manage the default network acl to detect drift
 resource "aws_default_network_acl" "this" {
+  count = var.create_vpc ? 1 : 0
+
   default_network_acl_id = aws_vpc.this[0].default_network_acl_id
 
   ingress {
@@ -46,4 +78,74 @@ resource "aws_default_network_acl" "this" {
   tags = {
     "Name" = "default"
   }
+}
+
+#
+# Subnets
+#
+resource "aws_subnet" "public" {
+  count = var.create_vpc && length(var.public_subnets) > 0 && (length(var.public_subnets) >= length(var.azs)) ? length(var.public_subnets) : 0
+
+  vpc_id                          = local.vpc_id
+  cidr_block                      = element(concat(var.public_subnets, [""]), count.index)
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id            = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  map_public_ip_on_launch         = var.map_public_ip_on_launch
+  assign_ipv6_address_on_creation = false
+
+  tags = merge(
+    {
+      "Name" = format(
+        "%s-${var.public_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags,
+    var.public_subnet_tags,
+  )
+}
+
+resource "aws_subnet" "private" {
+  count = var.create_vpc && length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  vpc_id                          = local.vpc_id
+  cidr_block                      = var.private_subnets[count.index]
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id            = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  assign_ipv6_address_on_creation = false
+
+  tags = merge(
+    {
+      "Name" = format(
+        "%s-${var.private_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags,
+    var.private_subnet_tags,
+  )
+}
+
+resource "aws_subnet" "isolated" {
+  count = var.create_vpc && length(var.isolated_subnets) > 0 ? length(var.isolated_subnets) : 0
+
+  vpc_id                          = local.vpc_id
+  cidr_block                      = var.isolated_subnets[count.index]
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id            = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  assign_ipv6_address_on_creation = false
+
+  tags = merge(
+    {
+      "Name" = format(
+        "%s-${var.isolated_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags,
+    var.isolated_subnet_tags,
+  )
 }
--- variables.tf
+++ variables.tf
@@ -17,6 +17,20 @@ variable "enable_dns_support" {
   default     = true
 }
 
+variable "enable_nat_gateway" {
+  description = "Controls if NAT gateway should be created"
+  type        = bool
+  default     = false
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Should be false if you do not want to auto-assign public IP on launch"
+  type        = bool
+  default     = true
+}
+
+# strings
+
 variable "name" {
   description = "Name to be used on all the resources as identifier"
   type        = string
@@ -29,6 +43,55 @@ variable "cidr" {
   default     = "0.0.0.0/0"
 }
 
+variable "secondary_cidr_blocks" {
+  description = "List of secondary CIDR blocks to associate with the VPC to extend the IP Address pool"
+  type        = list(string)
+  default     = []
+}
+
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = []
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "isolated_subnets" {
+  description = "A list of non-routable subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "public_subnet_suffix" {
+  description = "Suffix to append to public subnets name"
+  type        = string
+  default     = "public"
+}
+
+variable "private_subnet_suffix" {
+  description = "Suffix to append to private subnets name"
+  type        = string
+  default     = "private"
+}
+
+variable "isolated_subnet_suffix" {
+  description = "Suffix to append to outpost subnets name"
+  type        = string
+  default     = "isolated"
+}
+
+# maps
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
@@ -37,6 +100,24 @@ variable "tags" {
 
 variable "vpc_tags" {
   description = "Additional tags for the VPC"
+  type        = map(string)
+  default     = {}
+}
+
+variable "public_subnet_tags" {
+  description = "Additional tags for the public subnets"
+  type        = map(string)
+  default     = {}
+}
+
+variable "private_subnet_tags" {
+  description = "Additional tags for the private subnets"
+  type        = map(string)
+  default     = {}
+}
+
+variable "isolated_subnet_tags" {
+  description = "Additional tags for the isolated subnets"
   type        = map(string)
   default     = {}
 }
