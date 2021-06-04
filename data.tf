af4a39b982bf83ffe6ed3eaa0192d5c08c06a0b6
James Phillips <phillips.james@gmail.com>
2021-06-04 17:58:01 -0500

    added availability zones data source

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 # we attempt to be fully region independent
 data "aws_region" "current" {}
-data "aws_availability_zones" "available" {
+data "aws_availability_zones" "az" {
   state = "available"
 }
