25de9bb7c30526052104499abdcd5daa1b6c0750
James Phillips <phillips.james@gmail.com>
2021-06-05 08:28:32 -0500

    adding public subnet nacl

--- main.tf
+++ main.tf
@@ -215,23 +215,23 @@ resource "aws_network_acl" "public" {
   vpc_id     = element(concat(aws_vpc.this.*.id, [""]), 0)
   subnet_ids = aws_subnet.public.*.id
 
-  # ingress {
-  #   protocol   = -1
-  #   rule_no    = 100
-  #   action     = "allow"
-  #   cidr_block = "0.0.0.0/0"
-  #   from_port  = 0
-  #   to_port    = 0
-  # }
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 
-  # egress {
-  #   protocol   = -1
-  #   rule_no    = 100
-  #   action     = "allow"
-  #   cidr_block = "0.0.0.0/0"
-  #   from_port  = 0
-  #   to_port    = 0
-  # }
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 
 
   tags = merge(
