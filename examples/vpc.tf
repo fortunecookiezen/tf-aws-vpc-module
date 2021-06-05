4d95961a23782fb925be71b6d839fdc7536c614b
James Phillips <phillips.james@gmail.com>
2021-06-04 20:06:19 -0500

    adding logic to auto associate azs with subnets based on region

--- examples/vpc.tf
+++ examples/vpc.tf
@@ -1,9 +1,13 @@
 module "vpc" {
   source = "../"
-  name   = "vpc"
+  name   = "test"
   cidr   = "10.10.0.0/20"
 
   create_igw = false
+
+  public_subnets   = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  private_subnets  = ["10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24", "10.10.7.0/24"]
+  isolated_subnets = ["10.10.8.0/24", "10.10.9.0/24", "10.10.10.0/24", "10.10.11.0/24"]
 
   tags = {
     Owner       = "user"
@@ -11,6 +15,6 @@ module "vpc" {
   }
 
   vpc_tags = {
-    Name = "vpc-name"
+    PCI = "false"
   }
 }
